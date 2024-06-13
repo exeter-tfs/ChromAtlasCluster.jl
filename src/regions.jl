1b5f97d1d5b7a166eb31dbeb6b6b5b71d1452183
owensnick <owensnick@gmail.com>
2024-06-13 15:11:26 +0100

    added load regions

---
+++ src/regions.jl
@@ -0,0 +1,6 @@
+
+function loadrois()
+
+    regions = Dict("hk1_genelocus" => ("chr10", 69267991:69403882), "hk1_roi" => ("chr10", 69347894:69350486), "hk1_variants" => ("chr10", 69348854:69348961))
+
+end
